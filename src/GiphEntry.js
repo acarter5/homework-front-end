e5e42b34465f6c6cc9663c7c27b8320ec285aee3
Adam Carter <carter.c.adam@gmail.com>
2018-09-21 16:38:35 -0700

    trending giphs render on page load

---
+++ src/GiphEntry.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './GiphEntry.css';
+
+var GiphEntry = (props) => (
+  <div className='giph-entry'>
+    <img src={props.giph.images.fixed_width.url}/>
+  </div>
+);
+
+// PropTypes tell other developers what `props` a component expects
+// Warnings will be shown in the console when the defined rules are violated
+GiphEntry.propTypes = {
+  giph: React.PropTypes.object.isRequired
+};
+
+export default GiphEntry;
