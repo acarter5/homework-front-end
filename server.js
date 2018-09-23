629c76882a14441e98f19f5d2a977e2fece9d32d
Adam Carter <carter.c.adam@gmail.com>
2018-09-23 16:48:19 -0700

    backend operational

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('./database/operations.js');
 const app = express();
+
+app.use(function (req, res, next) {    
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
   return res.send('pong');
+});
+
+app.get('/favorites', function (req, res) {
+
+  db.getFavorites((err, data) => {
+    try {
+      res.status(200).send(data);
+    } catch (err) {
+      res.staus(404).send(err);
+    }  
+  });
 });
 
 app.get('/', function (req, res) {
